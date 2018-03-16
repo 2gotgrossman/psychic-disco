2549bb692b390a4697e3984963f226087ad511b4
David W Grossman <2gotgrossman@gmail.com>
2018-03-16 19:22:44 -0400

    handles "Open Tab in New Window" Case

--- background.js
+++ background.js
@@ -1,52 +1,72 @@
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-var SERVER_URL = "http://127.0.0.1:5000/";
+//****************************************************************************
+// 1. Identifying New URLs Code
+//****************************************************************************
 
 var selectedId = -1;
+var unvisitedTabs = new Set();
+
+// Keep track of opened, but unvisited tabs. The "Open Link in New Tab" Case
+chrome.tabs.onCreated.addListener(function(tab){
+  unvisitedTabs.add(tab.id);
+});
 
 
 chrome.tabs.onUpdated.addListener(function(tabId, props) {
   if (props.status == "complete" && tabId == selectedId){
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
       selectedId = tabs[0].id;
-      //alert(tabs[0].url);
-      postUrlToServer(SERVER_URL, tabs[0].url);
+      startPostUrlToServer(SERVER_URL, tabs[0].url);
     });
   }
 
 });
 
-chrome.tabs.onSelectionChanged.addListener(function(tabId, props) {
-  selectedId = tabId;
+chrome.tabs.onActivated.addListener(function(activeInfo) {
+  tabId = activeInfo.tabId;
+
+  // Covers case when Link was opened from another tab
+  // "Open Link in New Tab"
+  if ( unvisitedTabs.has(tabId) ) {
+    chrome.tabs.get(tabId, function(tab) {
+      startPostUrlToServer(SERVER_URL, tab.url);
+    });
+
+  }
+  unvisitedTabs.delete(tabId);
+  selectedId = activeInfo.tabId;
 });
 
-chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-  selectedId = tabs[0].id;
-});
+//****************************************************************************
+// Client REST API Logic
+//****************************************************************************
 
+var SERVER_URL = "http://127.0.0.1:5000/";
+
+/* contentType is not JSON because an initial `OPTIONS` request is sent if 
+ * it is not a "simple request". 
+ * More info here: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS#Simple_requests
+ */
+var contentType = "application/x-www-form-urlencoded;charset=UTF-8";
 var method = "POST";
-
 var shouldBeAsync = true;
 
 var request = new XMLHttpRequest();
 
-// Before we send anything, we first have to say what we will do when the
-// server responds. This seems backwards (say how we'll respond before we send
-// the request? huh?), but that's how Javascript works.
-// This function attached to the XMLHttpRequest "onload" property specifies how
-// the HTTP response will be handled. 
-
+startPostUrlToServer = function (server, url) {
+  if (url != "chrome://newtab/"){
+    postUrlToServer(server, url);
+  }
+}
 
 postUrlToServer = function (server, url) {
   request.open("POST", server, shouldBeAsync)
   request.onload = function () {
     var status = request.status; // HTTP response status, e.g., 200 for "200 OK"
-    alert(status);
+    //alert(status);
     var data = request.responseText; // Returned data, e.g., an HTML document.
   };
-  request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8");
+
+  request.setRequestHeader("Content-Type", contentType);
   request.send(url);
   //alert("Sent to server: " + server + " URL: " + url);
 }
